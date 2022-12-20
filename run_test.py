fdd4f4f036f5c1cdf6ed7c0208dd68894d0a8e21
Ao Li <leeleo3x@gmail.com>
2022-12-20 09:01:05 -0600

    update.
    update.

--- run_test.py
+++ run_test.py
@@ -17,13 +17,14 @@ def main():
 @main.command(name="build")
 def build():
     subprocess.call(["mvn", "install", "-DskipTests"], cwd=DIR)
-    subprocess.call(["java", "-cp", PHOSPHOR_JAR_PATH, "edu.columbia.cs.psl.phosphor.Instrumenter",
-                    "hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.4.0-SNAPSHOT-test-jar-with-dependencies.jar", "instrumented"], cwd=DIR)
 
 
 @main.command(name="instrument")
 def instrument():
-    subprocess.call(["java", "-cp", PHOSPHOR_JAR_PATH, "edu.columbia.cs.psl.phosphor.Instrumenter",
+    subprocess.call(["java",
+                     f"-DPhosphor.INSTRUMENTATION_CLASSPATH={INSTRUMENTATION_CLASSPATH}",
+                     f"-DPhosphor.ORIGIN_CLASSPATH={ORIGIN_CLASSPATH}",
+                     "-cp", PHOSPHOR_JAR_PATH, "edu.columbia.cs.psl.phosphor.Instrumenter",
                     "hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.4.0-SNAPSHOT-test-jar-with-dependencies.jar", "instrumented"], cwd=DIR)
 
 
@@ -36,14 +37,24 @@ def origin(debug: bool):
         command.insert(0, "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=5005")
     subprocess.call(["java"] + command)
 
+@main.command(name="static")
+def static():
+    # subprocess.call(["java",
+    #                  "-cp",
+    #                  "hadoop-hdfs-project/hadoop-hdfs/target/hadoop-hdfs-3.4.0-SNAPSHOT-test-jar-with-dependencies.jar",
+    #                  f"-javaagent:{RUNTIME_JAR_PATH}=static:{INSTRUMENTATION_CLASSPATH}",
+    #                  f"-agentpath:{NATIVE_LIB_PATH}=exchain:Lorg/apache/hadoop/hdfs",
+    #                  "org.apache.hadoop.hdfs.server.namenode.TestCheckpoint"])
+    subprocess.call(["./gradlew", "static-analyzer:run", f"--args={ORIGIN_CLASSPATH} {DIR}/static-results {ORIGIN_CLASSPATH}"], cwd=os.path.join(DIR, "../.."))
+
 
 @main.command(name="dynamic")
 def dynamic():
     subprocess.call([INSTRUMENTED_JAVA_EXEC,
                      "-cp", "instrumented/hadoop-hdfs-3.4.0-SNAPSHOT-test-jar-with-dependencies.jar",
                      f"-javaagent:{PHOSPHOR_AGENT_PATH}",
-                     f"-javaagent:{RUNTIME_JAR_PATH}",
-                     f"-agentpath:{NATIVE_LIB_PATH}=taint:Lorg/apache/hadoop/hdfs",
+                     f"-javaagent:{RUNTIME_JAR_PATH}=dynamic:{INSTRUMENTATION_CLASSPATH}",
+                     f"-agentpath:{NATIVE_LIB_PATH}=exchain:Lorg/apache/hadoop/hdfs",
                      "org.apache.hadoop.hdfs.server.namenode.TestCheckpoint"])
 
 
